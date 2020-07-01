e0ccc40190bbfcf367ddad48cb484d121eba76d9
neritoo <ezegavilan95@gmail.com>
2020-07-01 20:57:05 -0300

    SubredditService: SubredditDto guarda y devuelve el username.

--- src/main/java/com/gavilan/redditapirest/service/SubredditService.java
+++ src/main/java/com/gavilan/redditapirest/service/SubredditService.java
@@ -2,9 +2,13 @@ package com.gavilan.redditapirest.service;
 
 import com.gavilan.redditapirest.dto.SubredditDto;
 import com.gavilan.redditapirest.model.Subreddit;
+import com.gavilan.redditapirest.model.User;
 import com.gavilan.redditapirest.repository.SubredditRepository;
+import com.gavilan.redditapirest.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,18 +21,40 @@ import java.util.stream.Collectors;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
+
         Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
         subredditDto.setId(save.getId());
+        subredditDto.setUsername(save.getUser().getUsername());
 
         return subredditDto;
     }
 
+    private User getUserLogged() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        UserDetails userDetails = null;
+
+        if (principal instanceof UserDetails) {
+            userDetails = (UserDetails) principal;
+        }
+
+        assert userDetails != null;
+        String username = userDetails.getUsername();
+
+        return userRepository.findByUsername(username).orElse(null);
+
+    }
+
     private Subreddit mapSubredditDto(SubredditDto subredditDto) {
+        User userLogged = getUserLogged();
+
         return Subreddit.builder().name(subredditDto.getName())
                 .description(subredditDto.getDescription())
+                .user(userLogged)
                 .build();
     }
 
@@ -43,6 +69,7 @@ public class SubredditService {
     private SubredditDto mapToDto(Subreddit subreddit) {
         return SubredditDto.builder().name(subreddit.getName())
                 .id(subreddit.getId())
+                .username(subreddit.getUser().getUsername())
                 .numberOfPosts(subreddit.getPosts().size())
                 .build();
     }
