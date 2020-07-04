d25f0e826f0b65a0cae1d7e9be3b3d212209f212
neritoo <ezegavilan95@gmail.com>
2020-07-04 20:18:57 -0300

    TimeAgo posts en español.

--- src/main/java/com/gavilan/redditapirest/config/SecurityConfig.java
+++ src/main/java/com/gavilan/redditapirest/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.gavilan.redditapirest.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -27,6 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "api/subreddit")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
