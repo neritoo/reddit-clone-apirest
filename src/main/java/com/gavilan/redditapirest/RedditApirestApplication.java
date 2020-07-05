4a5dd4670b1ebbb6cb468828bf2824b459ec4604
neritoo <ezegavilan95@gmail.com>
2020-07-04 21:50:16 -0300

    Swagger documentation API.

--- src/main/java/com/gavilan/redditapirest/RedditApirestApplication.java
+++ src/main/java/com/gavilan/redditapirest/RedditApirestApplication.java
@@ -1,11 +1,14 @@
 package com.gavilan.redditapirest;
 
+import com.gavilan.redditapirest.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditApirestApplication {
 
     public static void main(String[] args) {
